ffb723e76aa64b1893dc7652a59a863984e47485
Santi <Santi@DESKTOP-2IHDDJI>
2021-04-16 20:23:25 -0300

    Inicializacion de Proyecto

---
+++ src/main/java/ar/edu/unju/edm/controller/ProductoController.java
@@ -0,0 +1,43 @@
+package ar.edu.unju.edm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ar.edu.unju.edm.model.Producto;
+import ar.edu.unju.edm.service.ProductoService;
+
+@Controller
+public class ProductoController {
+	
+	@Autowired
+	ProductoService iProductoService;
+	
+	@GetMapping("/producto")
+	public String cargarProducto(Model model) {
+		model.addAttribute("unProducto", iProductoService.obtenerProductoNuevo());
+		return("producto");
+	}
+
+	
+	@PostMapping("/producto")
+	public String guardarNuevoProducto(@ModelAttribute("unProducto") Producto nuevoProducto, Model model) {
+		iProductoService.guardarProducto(nuevoProducto);
+		model.addAttribute("productos", iProductoService.obtenerTodosProductos());
+		return "resultado";
+	}
+	
+	@GetMapping("/ultimo")
+	public String cargarUltimoProducto(Model model) {
+		model.addAttribute("ultimoProducto", iProductoService.obtenerUltimoProducto());
+		return("ultimo");
+	}
+	
+	@GetMapping("/volver")
+	public String cargarNuevoProducto(Model model) {
+		return("redirect:/producto");
+	}
+}
